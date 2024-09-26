21f40f35b16d12f7ea2236370daee911327900dc
Mateus Carvalho <mateusdev19@hotmail.com>
2024-09-26 17:35:39 -0300

    “first-commit“

---
+++ funcao/thisEBind1.js
@@ -0,0 +1,17 @@
+const pessoa = {
+    saudacao: 'Bom dia',
+    falar(){
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+const falar = pessoa.falar
+falar() // conflito entre paradigmas: funcional e OO
+
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()
+
+// A função BIND, somente é executada dentro da função de amarração do objeto
+const falar2 = pessoa.falar
+falar2()
